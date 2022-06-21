76897114e5e0e7d6c7fdb3bb31b07200fa9b4c34
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-06-21 13:50:48 +0200

    [TMP] cepheus: Inherit prebuilt kernel
    Change-Id: Id7c68dc06c446b66a960fb45ad9512bcde970907

--- device.mk
+++ device.mk
@@ -26,6 +26,9 @@ PRODUCT_PACKAGES += \
     libMegviiFacepp-0.5.2 \
     libmegface
 
+# Kernel
+$(call inherit-product-if-exists, device/xiaomi/cepheus-kernel/kernel.mk)
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
