8b1e1ddcf045364913a2b61cab114571fc6eaa00
LuK1337 <priv.luk@gmail.com>
2024-10-18 11:40:33 +0200

    nabu: Adjust override prop key names
    Change-Id: I436bbf425023dbe54cf3e2c35f50a5575511bc38

--- lineage_cepheus.mk
+++ lineage_cepheus.mk
@@ -23,6 +23,6 @@ PRODUCT_MODEL := MI 9
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="cepheus-user 11 RKQ1.200826.002 V12.5.1.0.RFAMIXM release-keys"
+    BuildDesc="cepheus-user 11 RKQ1.200826.002 V12.5.1.0.RFAMIXM release-keys"
 
 BUILD_FINGERPRINT := Xiaomi/cepheus/cepheus:11/RKQ1.200826.002/V12.5.1.0.RFAMIXM:user/release-keys
